5a0a05af6add84b4860c095431d457077cd352ec
Hyun-HaHa <whgusgk8@naver.com>
2023-10-08 00:07:15 +0900

    JSX문법 - 함수 사용

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const WINS = {
+    rock: 'scissor',
+    scissor: 'paper',
+    paper: 'rock',
+};
+  
+function getResult(left, right) {
+    if (WINS[left] === right) return '승리';
+    else if (left === WINS[right]) return '패배';
+    return '무승부';
+}
+
+function handleClick() {
+    console.log('가위바위보!');
+}
+  
+const me = 'rock';
+const other = 'scissor';
+
 root.render(
-  <p className="hello">안녕 리액트!</p>,
-  document.getElementById('root')
+    <>
+        <h1>가위바위보</h1>
+        <h2>{getResult(me, other)}</h2>
+        <button onClick={handleClick}>가위</button>
+        <button onClick={handleClick}>바위</button>
+        <button onClick={handleClick}>보</button>
+    </>
+    ,document.getElementById('root')
 );
 
