e12497c396e9c8c51e0e61cfbffb536433065e10
Hyun-HaHa <whgusgk8@naver.com>
2023-10-16 22:24:18 +0900

    가위바위보 - HandButton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import Dice from "./Dice";
-import HandIcon from "./HandIcon";
+// import HandIcon from "./HandIcon";
+import HandButton from './HandButton';
 
 function App() {
+
+    const handleClick = (value) => console.log(value);
+
     return (
         <div>
-            <HandIcon value="rock"/>
-            <HandIcon value="scissor"/>
-            <HandIcon value="paper"/>
+            <HandButton value="rock" onClick={handleClick}/>
+            <HandButton value="scissor" onClick={handleClick}/>
+            <HandButton value="paper" onClick={handleClick}/>
         </div>
     )
 }
