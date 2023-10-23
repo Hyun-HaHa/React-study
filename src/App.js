e4127492baa373618fc07bdf81525d38c940d4fc
Hyun-HaHa <whgusgk8@naver.com>
2023-10-23 21:11:55 +0900

    가위바위보 - 승부 기록

--- src/App.js
+++ src/App.js
@@ -19,30 +19,37 @@ function App() {
 
     const [hand, setHand] = useState(INITIAL_VALUE);
     const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
+    const [gameHistory, setGameHistory] = useState([]);
 
     const handleButtonClick = (nextHand) => {
 
         const nextOtherhand = generateRandomHand();
+        const nextHistoryItem = getResult(nextHand, nextOtherhand);
 
         setHand(nextHand);
         setOtherHand(nextOtherhand);
+        setGameHistory([...gameHistory, nextHistoryItem]);
     };
 
+    // 처음부터 버튼 - 초기화
     const handleClearClick = () => {
         setHand(INITIAL_VALUE);
         setOtherHand(INITIAL_VALUE);
+        setGameHistory([]);
     }
 
     return (
         <div>
             <Button onClick={handleClearClick}>처음부터</Button>
-            <p>{getResult(hand, otherHand)}</p>
 
             <div>
                 <HandIcon value={hand} />
                 VS
                 <HandIcon value={otherHand} />
             </div>
+
+            <p>승부 기록 : {gameHistory.join(', ')}</p>
+
             <div>
                 <HandButton value="rock" onClick={handleButtonClick} />
                 <HandButton value="scissor" onClick={handleButtonClick} />
